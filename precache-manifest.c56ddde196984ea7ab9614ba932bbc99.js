f90acd979b99e9a374112c4d18d16bdebd729d3f
Hyewon Jeong <hyewon@Hyewons-MacBook-Pro.local>
2020-02-26 11:50:27 +0900

    Updates

---
+++ precache-manifest.c56ddde196984ea7ab9614ba932bbc99.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7dd67ffa600f781ac51c304b77c8f7b7",
+    "url": "/beautiful/index.html"
+  },
+  {
+    "revision": "5e2cf197f81a47e5f67e",
+    "url": "/beautiful/static/css/2.06565635.chunk.css"
+  },
+  {
+    "revision": "5a3e2092e4ff045510b6",
+    "url": "/beautiful/static/css/main.af34b2a4.chunk.css"
+  },
+  {
+    "revision": "5e2cf197f81a47e5f67e",
+    "url": "/beautiful/static/js/2.58a0fdd5.chunk.js"
+  },
+  {
+    "revision": "8942ce9582771afefebb185956bb8394",
+    "url": "/beautiful/static/js/2.58a0fdd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5a3e2092e4ff045510b6",
+    "url": "/beautiful/static/js/main.a0f2375a.chunk.js"
+  },
+  {
+    "revision": "cbcba334aef474381c62",
+    "url": "/beautiful/static/js/runtime-main.3d048531.js"
+  },
+  {
+    "revision": "c57784d049a91ec1926a9b7d1d0f7413",
+    "url": "/beautiful/static/media/belle-beauty-JPvy3rrWSeM-unsplash.c57784d0.jpg"
+  },
+  {
+    "revision": "b149f9393b6c928b63dd9f0155bf3095",
+    "url": "/beautiful/static/media/curology-wK0h-mlvfuc-unsplash.b149f939.jpg"
+  }
+]);
